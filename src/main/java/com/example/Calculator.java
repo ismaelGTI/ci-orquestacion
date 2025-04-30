eed7e07e847442cca6522546e5589cb540c9a4a9
ismaelGTI <ismaelmarin11@uma.es>
2025-04-30 12:44:21 +0200

    ok

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,7 +1,13 @@
 package com.example;
 
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public class Calculator {
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public static void main(String[] args) {
+            Calculator calculator = new Calculator();
+            int result = calculator.add(2, 3);
+            System.out.println("Result: " + result);
+        }
     }
-}
