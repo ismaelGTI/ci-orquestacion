05941e6b7b3208c3ca51d53b948ac022c0a4346a
ismaelGTI <ismaelmarin11@uma.es>
2025-04-30 11:41:24 +0200

    ok3

---
+++ src/test/java/com/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+    @Test
+    public void testAlwaysTrue() {
+        assertTrue(true);
+    }
+}
